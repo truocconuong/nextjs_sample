c10800608052772dba2ec6a6665134d33578590f
manhch <manhchu@fetch.tech>
2021-07-03 12:21:29 +0700

    custom font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import { AppProps } from 'next/app';
 import React from 'react';
 import "../styles/antd.less";
-import GlobalBaseStyle from '../styles/GlobalStyle';
 import '../styles/index.scss'
 const App = ({ Component, pageProps }: AppProps) => (
   <>
-    <GlobalBaseStyle />
     <Component {...pageProps} />
     </>
 );
