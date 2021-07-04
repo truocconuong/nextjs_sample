462feadd69968df3ec5ed6c0559849015b74adda
duy <duynguyen99.work@gmail.com>
2021-07-05 00:26:38 +0700

    fix feedback width text field

--- app/generals/SelectField/index.tsx
+++ app/generals/SelectField/index.tsx
@@ -24,7 +24,7 @@ const SelectField = (props: SelectFieldPropsInterface) => {
             suffixIcon={selectProps?.value ? <SelectUp /> : <SelectDown />}
             {...selectProps}
             className={
-              selectProps?.value && !selectProps?.disabled ? " box-shadow " : ""
+              selectProps.className + (selectProps?.value && !selectProps?.disabled ? " box-shadow " : "")
             }
           >
             {children}
