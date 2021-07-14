892b7be780c613d0c692fe6a5de97eb778ca461f
manhch <manhchu@fetch.tech>
2021-07-14 22:25:04 +0700

    learning center

---
+++ pages/learning-center.tsx
@@ -0,0 +1,10 @@
+import LearningCenter from '@layout/LearningCenter';
+import React from 'react';
+
+function LearningCenterScreen(props) {
+    return (
+        <LearningCenter />
+    );
+}
+
+export default LearningCenterScreen;
