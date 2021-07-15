066c25b94263fe1de563d95c3d8615054c11009c
manhch <manhchu@fetch.tech>
2021-07-16 00:35:22 +0700

    update load landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,16 @@ import {Typography} from "antd";
 import ExampleToggle from "generals/Toggle/ExampleToggle";
 import InvestmentsLayout from "@layout/Investments";
 import BusinessInterestsLayout from "@layout/BusinessInterests";
+import Layout, { Content } from "antd/lib/layout/layout";
+import HomeLayout from "@layout/Home/HomeLayout";
 
 const {Title} = Typography;
 const App = () => (
-  <div
-  // style={{
-  //   display: "flex",
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  //   height: "100vh",
-  // }}
-  >
-    {/* <Title>Protect your legacy in Minutes</Title>
-    <Title level={2}>h2. Ant Design</Title>
-    <Title level={3}>h3. Ant Design</Title>
-    <Title level={4}>h4. Ant Design</Title>
-    <Title level={5}>h5. Ant Design</Title> */}
-
-    {/* <ExampleButton /> */}
-    <BusinessInterestsLayout />
-    {/* <ExampleToggle /> */}
-  </div>
+  <Layout>
+  {/* <Header style={{height:'80px'}}></Header> */}
+  <Content>
+    <HomeLayout/>
+  </Content>
+</Layout>
 );
 export default App;
