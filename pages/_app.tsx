0b84beaab3d0fab47eb68f6833b15829d8e401cb
duy <duynguyen99.work@gmail.com>
2021-07-16 00:19:14 +0700

    add condition render navigation bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,17 @@ import { AppProps } from "next/app";
 import React from "react";
 import "antd/dist/antd.css";
 import "../styles/index.scss";
-import { useRouter } from "next/dist/client/router";
+import { NextRouter, useRouter } from "next/dist/client/router";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 import NavigationBar from "generals/NavigationBar";
 import SizeBrowser from "generals/SizeBrowser";
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
+  const router: NextRouter = useRouter();
   return (
-    <Provider store={store}>
+     <Provider store={store}>
       <React.Fragment>
-        <NavigationBar />
+        {(router?.pathname === "/start-your-will" || router?.pathname === "/start-your-will-upload") && <NavigationBar />}
         <Component {...pageProps} key={router?.route} />
         <SizeBrowser />
       </React.Fragment>
